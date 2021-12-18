0b7bea8a1c5bf8aee95f6bf1646a6fa4fc44f414
Jonathan Schwender <schwenderjonathan@gmail.com>
2021-12-18 16:54:24 +0100

    Revert "remove -lSystem"
    This reverts commit 7b4e464a6629a0242e002280f78186651e612c17.

--- generator/src/subcommands/gen_cmake/platform.rs
+++ generator/src/subcommands/gen_cmake/platform.rs
@@ -45,6 +45,7 @@ impl Platform {
                 }
                 OS::MacOS => (
                     vec![
+                        "System".to_string(),
                         "resolv".to_string(),
                         "c".to_string(),
                         "m".to_string(),
